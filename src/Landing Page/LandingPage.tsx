a551ac9a90513536e44f80925266d1e576c68112
Varsh1009 <svarshn09@gmail.com>
2024-10-17 19:42:40 -0400

    updated

--- src/Landing Page/LandingPage.tsx
+++ src/Landing Page/LandingPage.tsx
@@ -4,6 +4,7 @@ import TableRow from "./tableRows";
 import { BsArrowUpRightSquare } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import LabWork from "../Labs";
+import landingImage from '../../assets/landing-image.jpg'; // Adjust the path as necessary
 
 const LandingPage = () => {
   return (
@@ -12,6 +13,9 @@ const LandingPage = () => {
         <h1>Shrivarshini Narayanan</h1>
         <h2>CS5610 - 20596</h2>
       </div>
+      
+      <img src={landingImage} alt="Landing" className="landing-image" />
+
       <div className="action-buttons">
         <a href="https://github.com/Varsh1009" className="button-link">
           <button className="button">
@@ -46,17 +50,17 @@ const LandingPage = () => {
         <tbody>
           <TableRow
             name="A1"
-            description="Created a landing page for CS5610 that will contain the hyperlinks to all the following assignments as well as implemented the lab work which includes getting familiar with headings, paragraphs, lists, links, tables and routing."
+            description="Created a landing page for CS5610 that will contain the hyperlinks to all the following assignments as well as implemented the lab work."
             url="https://kanbas1.netlify.app/"
           />
           <TableRow
             name="A2"
-            description="Gained familiarity with different concepts of CSS and Bootstrap through lab work and utilized these skills to style our website."
+            description="Gained familiarity with different concepts of CSS and Bootstrap through lab work."
             url="https://flourishing-bombolone-8dd3ab.netlify.app/"
           />
           <TableRow 
             name="A3" 
-            description="Gained familiarity with javascript concepts."
+            description="Gained familiarity with JavaScript concepts."
             url="https://remarkable-heliotrope-53f3e0.netlify.app/" 
           />
           <TableRow name="A4" description="To be Updated" url="#" />
