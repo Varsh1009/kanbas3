e081f42364d263cd6dcb1ac626743860e21d5c08
Varsh1009 <svarshn09@gmail.com>
2024-10-17 21:32:10 -0400

    updated landing

--- src/Landing Page/LandingPage.tsx
+++ src/Landing Page/LandingPage.tsx
@@ -1,9 +1,7 @@
 import "./LandingPage.css";
 import { FaGithub } from "react-icons/fa";
-import TableRow from "./tableRows";
 import { BsArrowUpRightSquare } from "react-icons/bs";
 import { Link } from "react-router-dom";
-import LabWork from "../Labs";
 import landingImage from '../../assets/landing-image.jpg'; // Adjust the path as necessary
 
 const LandingPage = () => {
@@ -17,57 +15,22 @@ const LandingPage = () => {
       <img src={landingImage} alt="Landing" className="landing-image" />
 
       <div className="action-buttons">
-        <a href="https://github.com/Varsh1009" className="button-link">
+        <a href="https://github.com/Varsh1009" className="button-link" target="_blank" rel="noopener noreferrer">
           <button className="button">
             Source Code Repository <FaGithub />
           </button>
         </a>
-        <Link to="/" className="button-link">
-          <button className="button">
-            Go to Landing Page <BsArrowUpRightSquare />
-          </button>
-        </Link>
-        <Link to="/labwork/*" className="button-link">
-          <button className="button">
-            Go to Lab Work <BsArrowUpRightSquare />
-          </button>
-        </Link>
         <Link to="/kanbas" className="button-link">
           <button className="button">
             Go to Kanbas <BsArrowUpRightSquare />
           </button>
         </Link>
+        <Link to="/labwork/*" className="button-link">
+          <button className="button">
+            Go to Labs <BsArrowUpRightSquare />
+          </button>
+        </Link>
       </div>
-      
-      <table>
-        <thead>
-          <tr>
-            <th>Assignment</th>
-            <th>Description</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>
-          <TableRow
-            name="A1"
-            description="Created a landing page for CS5610 that will contain the hyperlinks to all the following assignments as well as implemented the lab work."
-            url="https://kanbas1.netlify.app/"
-          />
-          <TableRow
-            name="A2"
-            description="Gained familiarity with different concepts of CSS and Bootstrap through lab work."
-            url="https://flourishing-bombolone-8dd3ab.netlify.app/"
-          />
-          <TableRow 
-            name="A3" 
-            description="Gained familiarity with JavaScript concepts."
-            url="https://remarkable-heliotrope-53f3e0.netlify.app/" 
-          />
-          <TableRow name="A4" description="To be Updated" url="#" />
-          <TableRow name="A5" description="To be Updated" url="#" />
-          <TableRow name="A6" description="To be updated" url="#" />
-        </tbody>
-      </table>
     </div>
   );
 };
